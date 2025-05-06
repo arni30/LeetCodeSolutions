ef1a7187344826b2c66df00a4523707a7caf9e7f
Oleksandr Arnopolin <arnopolin30@gmail.com>
2025-05-06 11:51:53 +0200

    Find the Index of the First Occurrence in a String

--- src/main/java/com/oarnopolin/leetcode/findtheindexofthefirstoccurrenceinastring/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/main/java/com/oarnopolin/leetcode/findtheindexofthefirstoccurrenceinastring/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,4 +1,33 @@
 package com.oarnopolin.leetcode.findtheindexofthefirstoccurrenceinastring;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.strStr("ssadbutsad", "sad"));
+        System.out.println(solution.strStr("leetcode", "leeto"));
+        System.out.println(solution.strStr("abc", "c"));
+    }
+
+    static class Solution {
+        public int strStr(String haystack, String needle) {
+            int n = haystack.length();
+            int k = needle.length();
+            for (int left = 0; left <= n - k; left++) {
+                int right = 0;
+                boolean flag = true;
+                while (right < k) {
+                    if (needle.charAt(right) != haystack.charAt(left + right)) {
+                        flag = false;
+                        break;
+                    }
+                    right++;
+                }
+                if (flag) {
+                    return left;
+                }
+            }
+            return -1;
+        }
+    }
 }
