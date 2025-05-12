f5b1585abae9aef4e5f7716b5131327348496a1e
Oleksandr Arnopolin <arnopolin30@gmail.com>
2025-05-12 14:06:00 +0200

    Solutions for Remove Duplicates from Sorted Array and Remove Duplicates from Sorted Array II

--- src/main/java/com/oarnopolin/leetcode/removeduplicatesfromsortedarray/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/oarnopolin/leetcode/removeduplicatesfromsortedarray/RemoveDuplicatesFromSortedArray.java
@@ -24,35 +24,46 @@ for (int i = 0; i < k; i++) {
     assert nums[i] == expectedNums[i];
 }
 If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
 */
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {1, 1, 1, 2, 2, 2}; // Input array
+        int[] nums = {1, 1, 1, 2, 2, 2, 3}; // Input array
         int[] expectedNums = {1, 2}; // The expected answer with correct length
 
         int k = solution.removeDuplicates(nums); // Calls your implementation
 
         assert k == expectedNums.length;
         for (int i = 0; i < k; i++) {
-            System.out.println(nums[i]);
+            System.out.print(nums[i] + ", ");
             assert nums[i] == expectedNums[i];
         }
     }
 
     static class Solution {
         public int removeDuplicates(int[] nums) {
-            int writeIndex = 1;
-            for (int readIndex = 1; readIndex < nums.length; readIndex++) {
-                if (nums[readIndex] != nums[readIndex - 1]) {
-                    nums[writeIndex] = nums[readIndex];
-                    writeIndex++;
+            int k = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    nums[k++] = nums[i];
                 }
             }
-
-            nums = new int[3];
-            System.out.println(Arrays.toString(nums));
-            return nums.length;
+            return k;
         }
     }
 }
